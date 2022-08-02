26125c94c624198d32230ec9ff17eaec6f2d49b2
reyno405 <reyno405@gmail.com>
2022-08-02 10:45:29 -0400

    this is a update with the java variables only

---
+++ script.js
@@ -0,0 +1,24 @@
+// Assignment code here
+
+var generateBtn = document.querySelector("#generate");
+
+//variables to be used
+var enter;
+var confirmCharacters;
+var confirmNumbers;
+var confirmUpper;
+var confirmLower;
+var userChoices = [];
+
+//Characters characters list
+var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "}", "~"];
+//number list
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var alphabet list
+var alphaUpper = ["A", "B","C","D","E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Lowercase alphabet list
+var alphaLower = alphaUpper.toString().toLocaleLowerCase();
+
+
+
+

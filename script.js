5cdf34daa65a0d38986073731507b3883ff466cb
reyno405 <reyno405@gmail.com>
2022-08-03 17:24:22 -0400

    in the update i added .join() to the end of the variables

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ var confirmLower;
 var userChoices = [];
 
 //Characters characters list
-var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "}", "~"];
+var characters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "}", "~"].join("");
 //number list
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].join("");
 // var alphabet list
-var alphaUpper = ["A", "B","C","D","E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaUpper = ["A", "B","C","D","E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"].join("");
 //Lowercase alphabet list
 var alphaLower = alphaUpper.toString().toLocaleLowerCase();
 
